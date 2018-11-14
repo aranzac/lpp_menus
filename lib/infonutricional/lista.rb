ed4f85602309c85cf8aafb2ad569b57a9707995d
aranza <aranza.1996@hotmail.com>
2018-11-14 13:08:12 +0000

    Corrección de los métodos insertar_tail y extraer_head. Las pruebas funcionan correctamente

--- lib/infonutricional/lista.rb
+++ lib/infonutricional/lista.rb
@@ -18,22 +18,22 @@ class Lista
         end
     end
     
-    def insertar_head(valor)
-        elemento = Node.new(valor,nil,@head)
+    def insertar_tail(valor)
+        elemento = Node.new(valor,@head,nil)
         if (@size == 0)
-          @head = Node.new(valor,nil,nil)
-          @tail = @head
+          @tail = Node.new(valor,nil,nil)
+          @head = @tail
           @size += 1
         elsif (@size == 1)
-            @head = elemento
-            @tail.next = @head
-            @head.prev = @tail
+            @tail = elemento
+            @head.next = @tail
+            @tail.prev = @head
             @size += 1
         elsif (@size > 1)
-            aux = Node.new(@head.value,elemento,@head.prev)
+            aux = Node.new(@tail.value,@tail.next,elemento)
             aux.prev.next = aux
-            @head = elemento
-            @head.prev.next = @head
+            @tail = elemento
+            @tail.next.prev = @tail
             @size += 1
         end
     end
@@ -43,14 +43,14 @@ class Lista
             puts "La lista está vacía."
         elsif (@size == 1)
             saliente = Node.new(@head.value,nil,nil)
-            @head = Node.new(nil,nil,nil)
             @tail = Node.new(nil,nil,nil)
+            @head = Node.new(nil,nil,nil)
             @size -= 1
             return saliente.value
         elsif (@size > 1)
             saliente = Node.new(@head.value,nil,nil)
-            @head.prev.next = nil
-            @head = @head.prev
+            @head.next.prev = nil
+            @head = @tail.next
             @size -= 1
             return saliente.value
         end 
--- spec/listas_spec.rb
+++ spec/listas_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Lista do
     @galletas = Etiqueta.new("Galletas", 25.0,12.0,63.0,29.0,6.3,0.6, 4,40)
     @arroz = Etiqueta.new("Arroz",1.1,0.3,74.0,0.0,8.4,0.0, 0,0)
     @pan_molde = Etiqueta.new("Pan de molde",3.4,0.43,45.0,4.6,7.0,1.5, 14,25)
+    @ketchup = Etiqueta.new("Ketchup", 0.1,0.0,24.8,19.0,1.6,3.3,0,0)
     @macarrones = Etiqueta.new("Macarrones", 1.9,0.8,71.8,3.5,11.5,0.08,0,0)
     @atun = Etiqueta.new("Atun", 31.0,4.6,0.9,0.5,19.0,0.9, 6,80)  
   end
@@ -18,7 +19,6 @@ RSpec.describe Lista do
       expect(@lista.empty).to eq(true)
       expect(@lista.head.value).to eq(nil)
       expect(@lista.tail.value).to eq(nil)
-      expect(@lista.head.next).to eq(nil)
       expect(@lista.size).to eq(0)
     end
   
@@ -27,46 +27,41 @@ RSpec.describe Lista do
       expect(@lista.tail.prev).to eq(nil)
     end
     
-    it "Existe el nodo final con next y prev vacios" do
-      expect(@lista.head.next).to eq(nil)
-      expect(@lista.tail.prev).to eq(nil)
-    end
-    
     it "Se pueden insertar elementos en la lista" do
-      @lista.insertar_head(@crema_Chocolate)
+      @lista.insertar_tail(@crema_Chocolate)
       expect(@lista.size).to eq(1)
-      @lista.insertar_head(@galletas)
+      @lista.insertar_tail(@galletas)
       expect(@lista.size).to eq(2)
-      @lista.insertar_head(@arroz)
+      @lista.insertar_tail(@arroz)
       expect(@lista.size).to eq(3)
-      @lista.insertar_head(@pan_molde)
+      @lista.insertar_tail(@ketchup)
       expect(@lista.size).to eq(4)
-      @lista.insertar_head(@atun)
+      @lista.insertar_tail(@pan_molde)
       expect(@lista.size).to eq(5)
-      @lista.insertar_head(@macarrones)
+      @lista.insertar_tail(@atun)
       expect(@lista.size).to eq(6)
+      @lista.insertar_tail(@macarrones)
+      expect(@lista.size).to eq(7)
     end
     
     it "La lista ya no es vacía" do
       expect(@lista.empty).to eq(false)
     end
     
-    it "El head es el ultimo elemento insertado" do
-      expect(@lista.head.value).to eq(@macarrones)
+    it "El head es el primer elemento insertado" do
+      expect(@lista.head.value).to eq(@crema_Chocolate)
     end
     
-    it "El tail es el primer elemento insertado" do
-      expect(@lista.tail.value).to eq(@crema_Chocolate)
+    it "El tail es el ultimo elemento insertado" do
+      expect(@lista.tail.value).to eq(@macarrones)
     end
 
-    it "Se puede extraer el último elemento insertadoo" do
-      expect(@lista.extraer_head()).to eq(@macarrones)
-      @lista.insertar_head(@macarrones)
+    it "Se puede extraer el primer elemento insertadoo" do
+      expect(@lista.extraer_head()).to eq(@crema_Chocolate)
     end
     
-    it("Comprobar que se acceden a los métodos de la clase infonutricional correctamente") do 
-      expect(@lista.extraer_head.sal).to eq(@macarrones.sal)
-      @lista.insertar_head(@macarrones)
+    it("Comprobar que se puede acceder a la sal del nodo") do 
+      expect(@lista.extraer_head.sal).to eq(@atun.sal)
     end
     
     it "Ver que el valor de sal es recomendado en todos los alimentos" do
@@ -78,11 +73,7 @@ RSpec.describe Lista do
         expect(@elemento3.sal).to be <= 6
         @elemento4 = @lista.extraer_head
         expect(@elemento4.sal).to be <= 6
-        @elemento5 = @lista.extraer_head
-        expect(@elemento5.sal).to be <= 6
-        @elemento6 = @lista.extraer_head
-        expect(@elemento6.sal).to be <= 6
+
     end
   end
-  
 end
